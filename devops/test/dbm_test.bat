c312fc3c3b5f05b742adf7af26964f8c105cce63
Brady Byrd <bradford.byrd@gmail.com>
2018-11-12 13:15:16 -0500

    Add batch file for groovy in test folder

---
+++ devops/test/dbm_test.bat
@@ -0,0 +1,9 @@
+echo off
+echo #--------------------------------------------#
+echo #      DBmaestro Processor         #
+echo #--------------------------------------------#
+java -cp ".;C:\Automation\dbm_demo\devops\lib\*;C:\Automation\dbm_demo\devops\lib\groovy-all-2.4.7.jar" groovy.ui.GroovyMain c:\Automation\dbm_demo\devops\test\update_package_order.groovy %*
+IF %ERRORLEVEL% NEQ 0 ( 
+   echo Groovy Script failed code: %ERRORLEVEL%
+   EXIT /b %ERRORLEVEL% 
+)
