1b33b6fcd111419912ea4d3921955713921e9691
Brady B <bradyb@dbmaestro.com>
2019-03-26 19:39:52 +0000

    Updates for local scripts

--- hr_demo/versions/0.0.4__ProjA/alter_name_A.sql
+++ hr_demo/versions/0.0.4__ProjA/alter_name_A.sql
@@ -1,1 +1,5 @@
-alter table bb_new alter column(name varchar2(120));
+alter table rsd_exrates.bb_new alter column(name varchar2(120));
+
+alter table rsd_rates.bb_new alter column(name varchar2(120));
+
+alter table rsd_ods.bb_new alter column(name varchar2(120));
